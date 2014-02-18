72994956aa9bcfd5b2233bf927f2d35ee2974100
victorfanneau <lahorie.victor@gmail.com>
2014-02-18 16:22:24 +0100

    voici un petit brouillon pour la banniere

---
+++ banniere.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Notre super bannière !</title>
+		<link rel="stylesheet" href="bannierestyle.css"/>
+	</head>
+	
+	<body>
+		<header class="header">
+			<img src="logo_1.1.jpg" alt="Tune In Town Society's logo"/>
+			<form class ="form" method="post" action="sign_in.php">
+				<span class="login">Pseudo : </span>
+				<input type="text" name="pseudo" value=""/>
+				<span class="login">Password : </span>
+				<input type="password" name="mdp" value=""/>
+				<input type="submit" value="Log in"/>
+				<span class="sign in"><a href ="notre page d'inscription">Inscription</a></span>
+			</form>
+		</header>
+	</body>
+</html>
