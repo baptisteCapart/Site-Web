b102b9252537539cfdc7e69b9eecbd30bbc5f550
victorfanneau <lahorie.victor@gmail.com>
2014-02-25 17:55:35 +0100

    repositionnement

---
+++ home.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Tune In Town</title>
+		<rel type="stylesheet" href=""/>
+	</head>
+
+	<body>
+		<?php include ?>
+	</body>
+</html>
