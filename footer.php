ce7a535328260e65285bad51ff3130e2634b6ecf
BenoitGuyot <benoit.guyot@isep.fr>
2014-03-06 18:53:59 +0100

    design footer + design menu deroulant

--- footer.php
+++ footer.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8"/>
-		<title>Tune In Town</title>
-		<link rel="stylesheet" href="bannierestyle.css" />
-	</head>
-	<body>
-		<footer>
-			<span class="like">Si tu trouves que TiT est cool, like nous sur Facebook et follow nous sur Twitter !</span>
-			<ul class ="help">
-				<li><a href="#">FAQ</a></li>
-				<li>Nous contacter : <a href="#">allo webmaster, bobo !</a></li>
-			</ul>
-		</footer>
-	</body>
-</html>
+
+<footer>
+	<div id="like">Si tu trouves que TiT est cool, like nous sur Facebook et follow nous sur Twitter !</div>
+	<ul id ="help">
+		<li><a href="#">FAQ</a></li>
+		<li><a href="#">Nous contacter</a></li>
+	</ul>
+</footer>
