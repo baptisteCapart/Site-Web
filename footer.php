340e95f989aaaafc6dc633d1b7b4bd09b9d7d8ec
victorfanneau <lahorie.victor@gmail.com>
2014-03-06 11:24:14 +0100

    foot

--- footer.php
+++ footer.php
@@ -3,12 +3,12 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>Tune In Town</title>
-		<rel type="stylesheet" href="bannierestyle.css" />
+		<link rel="stylesheet" href="bannierestyle.css" />
 	</head>
 	<body>
 		<footer>
-			<span>Si tu trouves que TiT est cool, like nous sur Facebook et follow nous sur Twitter !</span>
-			<ul>
+			<span class="like">Si tu trouves que TiT est cool, like nous sur Facebook et follow nous sur Twitter !</span>
+			<ul class ="help">
 				<li><a href="#">FAQ</a></li>
 				<li>Nous contacter : <a href="#">allo webmaster, bobo !</a></li>
 			</ul>
