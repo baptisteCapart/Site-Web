881312dec658eb6e1a3dc8463bd4a2c2fef68f15
javgoret <paul.heitz@hotmail.fr>
2014-03-10 18:51:27 +0100

    j'ai rajouté la bio+ changé l'image de stromae et pleins d'autres trucs

--- pageartiste.php
+++ pageartiste.php
@@ -15,6 +15,23 @@
 					Stromae
 				</div>
 			</div>
+			<div id="menudyna">
+				<a href="#">Présentation</a>
+				<a href="#">Concerts</a>
+				<a href="#">Extraits</a>
+				<a href="#">Avis</a>
+				<a href="#">Photos</a>
+			</div>
+			<div id="biographie">
+				<p>Stromae est né d'une mère belge et d'un père rwandais architecte, parti rapidement après sa naissance et qui ne l'a pas reconnu. Enfant, il ne le rencontre qu'à quelques reprises. Au milieu des années 1990, alors que la famille n'a plus de nouvelles du père, un appel leur annonce le décès de celui-ci quelques mois auparavant au cours du génocide rwandais. Sa mère élève seule à Bruxelles, puis en périphérie, une fratrie de quatre fils et une fille. À l'âge de onze ans, Stromae commence à s'intéresser à la musique et s'inscrit à l'Académie musicale de Jette pour prendre des cours de solfège et de batterie. Sa mère l'envoie à l'internat jésuite de Godinne.
+				</p>
+				<p>En 2000, il choisit le pseudonyme Opsmaestro pour commencer dans le monde du rap. Il explique dans une interview que l'album Temps mort du rappeur français Booba l'a beaucoup influencé. Son pseudonyme étant similaire à celui d'un autre artiste, il en change et opte pour Stromae, qui correspond à maestro en verlan. À dix-huit ans, il forme le groupe Suspicion en compagnie de JEDI, un autre rappeur, et ils composent ensemble la chanson et le clip de Faut qu't'arretes le rap…. JEDI décide néanmoins de quitter le duo, ce qui marque les débuts de la carrière solo de Stromae. En 2005, Stromae enchaîne les prestations. Puis, il participe à la Hip-Hop Family en 2006, ainsi qu'au Juste debout Benelux en 2007.
+				</p>
+				<p>Il travaille pendant un an dans la chaîne de restauration rapide Quick afin de financer ses études dans une école de cinématographie. Ses économies étant insuffisantes, il s'inscrit à l'INRACI section cinéma, il a mené des études d'ingénieur du son. Il investit son argent pour réaliser son premier opus, un maxi de quatre titres : Juste un cerveau, un flow, un fond et un mic…. Il collabore avec plusieurs rappeurs comme BdBanx et Beretta sur la compilation Dès le départ, ou encore Kery James.
+				</p>
+				<p>En 2008, il signe en édition chez Because Music et Kilomaître pour quatre ans. Il compose alors le single Cette fois interprété par Melissa M sur l'album Avec tout mon amour, réalise quatre compositions sur l'album À l'ombre du show business de Kery James dont la chanson Ghetto ainsi que la chanson Si je t'emmène de Anggun sur l'album Elevation6. Parallèlement, il diffuse des vidéos sur internet où il explique ses créations musicales sous forme de leçons appelées Les leçons de Stromae. Pour les compositions de ces leçons, il utilise le logiciel Reason.
+				</p>
+			</div>
 
 		<?php include("footer.php"); ?>
 	</body>
