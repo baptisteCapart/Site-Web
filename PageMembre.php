634cd73d4d776ab45f32047d9e4e75b005efe19c
vcordier <v.cordier.isep@gmail.com>
2014-03-11 09:27:18 +0100

    debut pageMembre

---
+++ PageMembre.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Page membre</title>
+		<rel type="stylesheet" href="" />
+		<link rel="stylesheet" href="bannierestyle.css"/>
+		<link rel="stylesheet" href="pageartistestyle.css"/>
+	</head>
+	<body>
+		<?php include("banniere.php"); ?>
+		<div id= "photoMembre">
+		</div>
+
+
+
+		<?php include("footer.php"); ?>
+		
+	</body>
+</html>
