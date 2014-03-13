71cd01fd9b37e84ef68a2e21d9ea1c20252cd6db
baptisteCapart <bc.diam@free.fr>
2014-03-13 11:51:54 +0100

    optpicture

--- PageMembre.php
+++ PageMembre.php
@@ -9,10 +9,20 @@
 	</head>
 	<body>
 		<?php include("banniere.php"); ?>
+
 		<div id= "cover">
-			<div id= "membre">
-			<img src="images/membreexemple.jpg" width="220" height="220" alt="Photo de Gérard" />
-			</div>
+			<ul>
+				<li>
+					<li><div id= "membre">
+					<img src="images/membreexemple_opt.jpg" width="220" height="220" alt="Photo de Gérard" />
+					</div></li>
+				</li>
+				<li>
+					<li></li>
+					<li></li>
+				</li>
+			</ul>
+			
 		</div>
 
 
