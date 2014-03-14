38278bea89132abc9660fe57b679df3e5f08d81d
baptisteCapart <bc.diam@free.fr>
2014-03-14 21:15:32 +0100

    pagemembre

--- PageMembre.php
+++ PageMembre.php
@@ -1,37 +1,26 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8"/>
-	<title>page membre</title>
-	<rel type="stylesheet" href="" />
-	<link rel="stylesheet" href="bannierestyle.css"/>
-	<link rel="stylesheet" href="pageMembrestyle.css"/>
-</head>
-<body>
-	<?php include("banniere.php"); ?>
 
-	<div id= "cover">
-		<ul id="membre">
-			<li>
+<?php include("banniere.php"); ?>
 
-					<img src="images/membreexemple_opt.jpg" width="220" height="220" alt="Photo de Gérard" />
-				
-			</li>
-			<li>
-				<ul id="donneesmembre">
-					<li>Gégé44</li>
-					<li>Homme</li>
-					<li>25/10/1965</li>
-					<li>Avignon</li>
-				</ul>
-			</li>
-		</ul>
+<div id= "cover">
+	<ul>
+		<li><div  id="membre">
 
-	</div>
+			<img src="images/membreexemple_opt.jpg" width="220" height="220" alt="Photo de Gérard" />
+</div>
+		</li>
+		<li>
+			<ul id="donneesmembre">
+				<li>Gégé44</li>
+				<li>Homme</li>
+				<li>25/10/1965</li>
+				<li>Avignon</li>
+			</ul>
+		</li>
+	</ul>
+
+</div>
 
 
 
-	<?php include("footer.php"); ?>
+<?php include("footer.php"); ?>
 
-</body>
-</html>
