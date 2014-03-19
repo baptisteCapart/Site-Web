b5183af4a088b00144aafcf017a23811e8899085
BenoitGuyot <benoit.guyot@isep.fr>
2014-03-19 15:04:43 +0100

    modele mvc pour le formulaire donc avec le modele la vue et le controleur plus un fichier de connecion a la bdd

--- formulairecontrolleur.php
+++ formulairecontrolleur.php
@@ -1,10 +1,9 @@
 <?php
-include('formulaire.php');
-if(!empty($_POST['pseudo']) AND !empty ($_POST['mdp']) AND !empty ($_POST['mdp2']) AND !empty ($_POST['mail']) AND !empty ($_POST['ville']) AND !empty ($_POST['codepostal']) AND !empty ($_POST['pays']) AND !empty ($_POST['sexe']) AND !empty ($_POST['photodecover']) AND !empty ($_POST['photodeprofil']))
+
+if(!empty($_POST['pseudo']) AND !empty ($_POST['mdp']) AND !empty ($_POST['mdp2']) AND !empty ($_POST['mail']) AND !empty ($_POST['ville']) AND !empty ($_POST['codepostal']) AND !empty ($_POST['pays']) AND !empty ($_POST['sexe']) )
 {
 // D'abord, je me connecte à la base de données.
-	mysql_connect("localhost", "root", "");
-	mysql_select_db("mydb");
+require ('connectbdd.php');
 
 // Je mets aussi certaines sécurités ici…
 	$mdp = mysql_real_escape_string(htmlspecialchars($_POST['mdp']));
@@ -18,18 +17,29 @@ if(!empty($_POST['pseudo']) AND !empty ($_POST['mdp']) AND !empty ($_POST['mdp2'
 		$codepostal = mysql_real_escape_string(htmlspecialchars($_POST['codepostal']));
 		$pays = mysql_real_escape_string(htmlspecialchars($_POST['pays']));
 		$ville = mysql_real_escape_string(htmlspecialchars($_POST['ville']));
-		$photodeprofil = mysql_real_escape_string(htmlspecialchars($_POST['photodeprofil']));
-		$photodecover = mysql_real_escape_string(htmlspecialchars($_POST['photodecover']));
+		$photodeprofil = "";
+		$photodecover = "";
+		if(!empty($_POST['photodeprofil'])){
+			$photodeprofil = mysql_real_escape_string(htmlspecialchars($_POST['photodeprofil']));
+		}
+		if(!empty($_POST['photodecover']))
+			{$photodecover = mysql_real_escape_string(htmlspecialchars($_POST['photodecover']));
+	} 	
 
 // Je vais crypter le mot de passe.
-		$mdp = sha1($mdp);
+	$mdp = sha1($mdp);
 
-		mysql_query("INSERT INTO membre(pseudo, mot_de_passe, mail, code_postal, ville, sexe, pays, photoprofil, photocover )  VALUES ('$pseudo', '$mdp', '$mail' ,'$codepostal', '$ville' ,'$sexe', '$photodeprofil', '$photodecover')");
-	}
+	require ('formulairemodel.php');
+	insert($pseudo, $mdp, $mail ,$codepostal, $ville ,$sexe, $pays,$photodeprofil, $photodecover);
+}
 
-	else
-	{
-		echo '<span class="erreur">Les deux mots de passe que vous avez rentrés ne correspondent pas</span>';
-	}
+else
+{
+	echo '<span class="erreur">Les deux mots de passe que vous avez rentrés ne correspondent pas</span>';
+}
+include ('formulaire.php');
+}else{
+
+include('formulaire.php');
 }
 ?>
