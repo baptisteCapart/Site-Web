5c41145c23289f49c737fa0c2b90cc880fa5cb67
BenoitGuyot <benoit.guyot@isep.fr>
2014-04-04 14:25:30 +0200

    liste atiste ok

--- views/listeartistes.php
+++ views/listeartistes.php
@@ -1,46 +1,49 @@
 
 <?php include("views/banniere.php"); ?>
-	<?php
+<!-- 	<?php
 	if(!isset($_GET['section']))
 		{$_GET['section'] = "listealpha";}
-	?>
+	?> -->
 	<div id="listeartiste">
 		<ul>
 			
-			<li class = "paralpha"><a href="#?section=listealpha">Par ordre alphabétique </br></a></li>
+			<li class = "paralpha"><a href="#?section=listealpha">Par ordre alphabétique </br></a>
+				</li>
 
 			<li class = "parstyle"><a href="#?section=listegenre">Par style</br></a></li>	
 		
 		</ul> 	
 
+
 	</div>
-	<div class="blocs">
-		
-		<div class="listealpha" style=
-			<?php 
-			if($_GET['section']=="listealpha")
-				{echo '"z-index : 50;"';}
-			else
-				{echo '"z-index : 0;"';}
-			?>
-		>
-			<?php
+	<div class = "contenuliste">			<?php
 				foreach ($listegroupe as $group)
 				{echo '<a href = "index.php?page=pageartistecontrolleur&id='.$group["artiste_id"].'">'. $group["nom"].'<br/>'.' </a>';}
-			?>
-		</div>
+			?></div>
+<!-- 	<div class="blocs">
+		
+		<div class="listealpha" style="
+			<?php 
+			if($_GET['section']=="listealpha")
+				{echo 'z-index : 50;';}
+			else
+				{echo  'z-index : 0;';}
+			?>"
+		> --> 
 
-		<div class="listegenre" style=
+		<!-- </div>
+
+		<div class="listegenre" style="
 			<?php 
 			if($_GET['section']=="listegenre")
-				{echo '"z-index : 50;"';}
+				{echo 'z-index : 50;';}
 			else
-				{echo '"z-index : 0;"';}
-			?>
+				{echo 'z-index : 0;';}
+			?>"
 		>
 			Tout plein de genres de musiques différents !
 		</div>
 
-	</div>
+	</div> -->
 		
 <?php include("views/footer.php"); ?>
