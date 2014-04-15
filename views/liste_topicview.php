1b2833ea28ef0e22b0ac31408cd6a8cc231edc6d
BenoitGuyot <benoit.guyot@isep.fr>
2014-04-15 13:03:49 +0200

    reindent

--- views/liste_topicview.php
+++ views/liste_topicview.php
@@ -2,13 +2,9 @@
 	<h1>Général</h1>
 
 	<div id= "listediscuss">
-
-	<ul class="discussions">
-	
-			<li><a href="index.php?page=discusscontroleur"> Le rap c trop cool </a></li>
-			<li>Rambouillet c trop cool</li>
-
-	</ul>
-
+		<ul class="discussions">
+				<li><a href="index.php?page=discusscontroleur"> Le rap c trop cool </a></li>
+				<li>Rambouillet c trop cool</li>
+		</ul>
 	</div>
 </div>
