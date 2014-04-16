4d2aba206f75ced4507ecfecf515e232df737337
BenoitGuyot <benoit.guyot@isep.fr>
2014-04-17 00:25:12 +0200

    tableau pour referencer les topics

--- controlleurs/liste_topiccontrolleur.php
+++ controlleurs/liste_topiccontrolleur.php
@@ -5,6 +5,7 @@
 include("controlleurs/bannierecontrolleur.php");
 include ('models/forummodel.php');
 $topicList=listeTopic();
+var_dump($topicList);
 include("views/liste_topicview.php");
 include("views/footer.php");
 
--- views/liste_topicview.php
+++ views/liste_topicview.php
@@ -1,12 +1,35 @@
 <div class="generaldiscuss">
 	<h1>Général</h1>
 
-	<div id= "listediscuss">
+<!-- 	<div id= "listediscuss">
 		<ul class="discussions">
 			<?php foreach ($topicList as $topic)
 			{echo ' <li><a href = "index.php?page=discusscontroleur&topic='.$topic["id_topic"].'">'. $topic["nom"].'<br/>'.' </a></li>';}?>
 		</ul>
-	</div>
+	</div> -->
+
+
+
+
+	<table>
+		<tr>
+	       <th>Nom</th>
+	       <th>Description</th>
+	       <th>nombre de messages</th>
+	       <th>Crée par</th>
+   		</tr>
+
+   		<?php foreach ($topicList as $topic) { var_dump($topic);?>
+	   	<tr>
+
+	       <td> <?php echo '<a href = "index.php?page=discusscontroleur&topic='.$topic["id_topic"].'">'. $topic["nom"].'<br/>'.' </a>';?></td>
+	       <td><?php echo '<a href = "index.php?page=discusscontroleur&topic='.$topic["id_topic"].'">'. $topic["description"].'<br/>'.' </a>';?></td>
+	       <td>3</td>
+	       <td>Xavier</td>
+	   </tr>
+	   <?php }  ?>
+
+	</table>
 
 	<div class="nouveau"><a href="index.php?page=new_topiccontroleur">Ton propre topic, c'est ici !</a></div>
 </div>
