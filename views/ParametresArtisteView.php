49077e01025af14a4690d7964a2481017d196bb9
BenoitGuyot <benoit.guyot@isep.fr>
2014-04-21 05:19:46 +0200

    pre-remplissage de la boite de texte pour la description

--- views/ParametresArtisteView.php
+++ views/ParametresArtisteView.php
@@ -14,7 +14,7 @@
 					<div class="style"><span>Style de musique : </span><input class = "textbox" type="text" name="style" value=""/></div>			
 				</li>
 				<li>
-					<div class="description"><span>Description : <TEXTAREA name="description" rows=10 COLS=40 value = "<?=$donnees['description'] ?>"></TEXTAREA></div>			
+					<div class="description"><span>Description : <TEXTAREA name="description" rows=10 COLS=40 ><?=$donnees['description'] ?></TEXTAREA></div>			
 				</li>				
 				<li>
 					<div class="photogroupe"><span>Photo du groupe : </span><input class = "textbox" type="file" name="photogroupe" /></div>
