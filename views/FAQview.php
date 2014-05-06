15a7a0c795a1f860eb10d75cee7ef220ba34c390
vcordier <v.cordier.isep@gmail.com>
2014-05-06 10:52:29 +0200

    forumchange

---
+++ views/FAQview.php
@@ -0,0 +1,13 @@
+<div id="listeFAQ">
+	
+	<div id="introduction">
+		<h1>F.A.Q</h1>
+		
+		<p>Bienvenue sur la F.A.Q de Tune in Town.<br>
+		    Ici vous trouverez divers renseignements sur les différentes fonctionalitées de notre site.<br>
+		     Si vous êtes nouveau et que vous vous y perdez un petit peu n'hésitez pas à jeter un petit<br>
+		     coup d'oeil.</p>
+	
+	</div>
+
+</div> 
