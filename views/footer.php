840da4e673f86d694fe8a21570d62fc4392e2c76
vcordier <v.cordier.isep@gmail.com>
2014-05-06 11:16:32 +0200

    faq

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<!-- <div id="like">Si tu trouves que TiT est cool, like nous sur Facebook et suit nous sur Twitter !</div> -->
 	<ul id ="help">
-		<li><a href="#">FAQ</a></li>
+		<li><a href="index.php?page=FAQcontrolleur">FAQ</a></li>
 		<li><a href="#">Nous contacter</a></li>
 	</ul>
 </footer>
