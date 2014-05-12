e513a9146fe4c7d657a82e2c91f677bb2c14c7d1
vcordier <v.cordier.isep@gmail.com>
2014-05-12 10:40:26 +0200

    faqsuite

--- views/FAQview.php
+++ views/FAQview.php
@@ -10,4 +10,23 @@
 	
 	</div>
 
+
+	<p>Comment me déconnecter de votre site ?<br>
+		<br>
+		Facile, il suffit de cliquer sur le bouton déconnexion après vous être connecté.<br>
+		<br>
+		<br>
+		Comment faire pour créer un compte membre sur votre site ?<br>
+		<br>
+		Sur la bannière du site vous pouvez cliquez sur le bouton d'inscription pour accéder à un petit<br>
+		 formulaire qui une fois rempli vous permettra d'avoir votre propre compte sur notre site.<br>
+		 <br>
+		 L'inscription sur votre site est-elle payante ?<br>
+		 Malheureusement non.<br>
+		 <br>
+		 Je gère une salle sur votre site et j'aimerais contacter un des artistes présent sur le site<br>
+		 comment faire ?<br>
+		 Pour ce faire appuyer sur le bouton invité sur la page de cet artiste et suivez les indications.
+	</p>
+
 </div> 
