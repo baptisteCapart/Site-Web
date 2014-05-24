b9a3310086349a09180181b9fd3c3c1d879beb54
victorfanneau <lahorie.victor@gmail.com>
2014-05-24 03:31:26 +0200

    creation news concert

--- models/concertmodel.php
+++ models/concertmodel.php
@@ -1,4 +1,6 @@
 <?php 
+include("artistemodel.php");
+
 function insertConcert($nom, $jour ,$description, $début, $duree, $message, $photocover, $salle_id, $artiste_id, $inviteur,$non_repondu)
 {
 	global $bdd;
@@ -77,10 +79,12 @@ function updateConcert($nom, $jour ,$description, $début, $duree, $message, $ph
 
 function accord($id, $accord){
 	global $bdd;
-	$bdd->query("UPDATE concert 
-		SET accord='$accord'
-
-		WHERE concert_id='$id'");
+	$bdd->query("UPDATE concert SET accord='$accord' WHERE concert_id='$id'");
+	$concert = recuperer5($id);
+	$artiste = recuperer2($concert['artiste_id']);
+	$description = "Tout nouveau concert de ".$artiste['nom']." prévu pour ".$concert['jour'].", tenez-vous prêts !";
+	$bdd->query("INSERT INTO news (datenews, photocover, description, lien) 
+		VALUES ("$concert['jour'].", ".$concert['photocover'].", $description, 'index.php?page=pageconcertcontrolleur&id='".$concert['concert_id'].")");
 }
 
 function newtopic ()
