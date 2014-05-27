522ea2a894ed3ddc4b85ea8834673aec295f670f
BenoitGuyot <benoit.guyot@isep.fr>
2014-05-27 23:50:06 +0200

    idem

--- controlleurs/formulairegroupecontrolleur.php
+++ controlleurs/formulairegroupecontrolleur.php
@@ -14,17 +14,21 @@ if(!empty($_POST['nomartiste']) AND !empty ($_POST['description']) AND !empty ($
 		}
 
 		include ('models/artistemodel.php');
-		global $choix;
+		if(isset($_SESSION['id'])){
+			
 		if(!empty($_POST['style'])){
+			$current_id = insertArtiste($nomartiste, $style ,$description, $photogroupe, $_SESSION['id']);
 			for ($i=0;$i<count($_POST['style']);$i++){
-				$choix .= $_POST['style'][$i];
+				$choix = $_POST['style'][$i];
+				finishartiste($current_id, $choix);
 			}
+			header ('location: index.php?page=pageartistecontrolleur&id='.$current_id);
 		}
 
-		if(isset($_SESSION['id'])){
-			$current_id = insertArtiste($nomartiste, $style ,$description, $photogroupe, $_SESSION['id']);
-			finishartiste($current_id, $choix);
-			header ('location: index.php?page=pageartistecontrolleur&id='.$current_id);
+		
+			
+			
+			
 		}
 
 		include('controlleurs/bannierecontrolleur.php');
