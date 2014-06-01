e40cb9fbcdf5e9b1a4d22e1acf989396ba7782cd
BenoitGuyot <benoit.guyot@isep.fr>
2014-06-01 18:20:19 +0200

    traitement des photos artiste

--- controlleurs/formulairegroupecontrolleur.php
+++ controlleurs/formulairegroupecontrolleur.php
@@ -5,7 +5,6 @@ if(!empty($_POST['nomartiste']) AND !empty ($_POST['description'])  AND !empty($
 
 		$style = mysql_real_escape_string(htmlspecialchars($_POST['style']));	
 		$nomartiste = mysql_real_escape_string(htmlspecialchars($_POST['nomartiste']));
-		$photogroupe = mysql_real_escape_string(htmlspecialchars($_POST['photogroupe']));
 		$description = mysql_real_escape_string(htmlspecialchars($_POST['description']));
 		$photogroupe ="";
 		
@@ -16,39 +15,32 @@ if(!empty($_POST['nomartiste']) AND !empty ($_POST['description'])  AND !empty($
 		}
 
 		include ('models/artistemodel.php');
-		if(isset($_SESSION['id'])){
-			$current_id = insertArtiste($nomartiste, $style ,$description, $photogroupe, $_SESSION['id']);
-			$nomInit = $_FILES['photogroupe']['name'];
-			$infosPath = pathinfo($nomInit);
-			$extension = $infosPath['extension'];
-			$extensionsAutorisees = array("jpeg", "jpg", "gif", "png");
-			$nomDestination = $current_id.".".$extension;
-			var_dump($infosPath);
-			if (!(in_array($extension, $extensionsAutorisees))) {
-				$messageA = "ATTENTION : le format de votre photo n'est pas bon, vous avez bien été insrit mais votre photo d'artiste sera générée par défaut";
-				$_SESSION['formatA'] = $messageA;
-			} else { 
-				photoA($current_id, $nomDestination);   
+		if(isset($_SESSION['id'])){	
+			if(!empty($_POST['style'])){
+				$current_id = insertArtiste($nomartiste, $style ,$description, $photogroupe, $_SESSION['id']);
+				$nomInit = $_FILES['photogroupe']['name'];
+				$infosPath = pathinfo($nomInit);
+				$extension = $infosPath['extension'];
+				$extensionsAutorisees = array("jpeg", "jpg", "gif", "png");
+				$nomDestination = $current_id.".".$extension;
+				var_dump($infosPath);
+				if (!(in_array($extension, $extensionsAutorisees))) {
+					$messageA = "ATTENTION : le format de votre photo n'est pas bon, vous avez bien été insrit mais votre photo d'artiste sera générée par défaut";
+					$_SESSION['formatA'] = $messageA;
+				} else { 
+					photoA($current_id, $nomDestination);   
 
-				$repertoireDestination = dirname(dirname(__FILE__))."/"."controlleurs"."/"."images"."/"."artistes"."/"; 
-				//   $nomDestination = "fichier_du_".date("YmdHis").".".$extensionFichier;
+					$repertoireDestination = dirname(dirname(__FILE__))."/"."controlleurs"."/"."images"."/"."artistes"."/"; 
+					//   $nomDestination = "fichier_du_".date("YmdHis").".".$extensionFichier;
 
-				move_uploaded_file($_FILES["photogroupe"]["tmp_name"], $repertoireDestination.$nomDestination);
-
-
-			}		
-		if(!empty($_POST['style'])){
-				
-			for ($i=0;$i<count($_POST['style']);$i++){
-				$choix = $_POST['style'][$i];
-				finishartiste($current_id, $choix);
-			}
-			header ('location: index.php?page=pageartistecontrolleur&id='.$current_id);
-		}
-
-		
-			
-			
+					move_uploaded_file($_FILES["photogroupe"]["tmp_name"], $repertoireDestination.$nomDestination);	
+				}				
+				for ($i=0;$i<count($_POST['style']);$i++){
+					$choix = $_POST['style'][$i];
+					finishartiste($current_id, $choix);
+				}
+				header ('location: index.php?page=pageartistecontrolleur&erreur="le format de votre fichier est non valide, votre photo sera générée par defaut"&id='.$current_id);
+			}	
 			
 		}
 
