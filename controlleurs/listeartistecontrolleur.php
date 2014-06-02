87e0d86aa6815577593bca765318e2722576d249
victorfanneau <lahorie.victor@gmail.com>
2014-06-02 18:42:51 +0200

    liste artistes par styles

--- controlleurs/listeartistecontrolleur.php
+++ controlleurs/listeartistecontrolleur.php
@@ -2,21 +2,26 @@
 
 include ('models/artistemodel.php');
 
-$notes = classement();
-
 $critere =1;
 
 if(isset($_GET['critere'])){
 
 	if ($_GET["critere"] ==1){
 		$critere=1;
+		$listealpha = trialpha();
 	}	
 	if ($_GET["critere"] ==2){
 		$critere=2;
+		$styles = getStyle();
 	}
 	if ($_GET["critere"] ==3){
 		$critere=3;
+		$notes = classement();
 	}
+}
+else 
+{
+	$listealpha = trialpha();
 }
 
 
