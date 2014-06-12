3a59e0b889cfc56a66b0ad2fb09da76b30a8f33d
BenoitGuyot <benoit.guyot@isep.fr>
2014-06-12 15:49:29 +0200

    fichiers supprimés à la racine

--- RandColor.php
+++
@@ -1,6 +0,0 @@
-<?php 
-function coul_aleat(){
- $couleur = array('#96F10E','#07CDCD','#D8551D','#EF3858','#843DFA','#F411E3','#EFA50A','#47993E');
-$valeur = rand(0, 6);
-return $couleur[$valeur];}
-?>
